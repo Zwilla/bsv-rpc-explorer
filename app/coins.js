9752d10b6bf2e06be96df35bb47073e34af2d17f
Zwilla <info@zwilla.de>
2019-02-14 11:01:16 +0100

    bsv like

--- app/coins.js
+++ app/coins.js
@@ -1,9 +1,12 @@
-var btc = require("./coins/btc.js");
-var ltc = require("./coins/ltc.js");
+//var btc = require("./coins/btc.js");
+//var ltc = require("./coins/ltc.js");
+var bsv = require("./coins/bsv.js");
 
 module.exports = {
-	"BTC": btc,
-	"LTC": ltc,
+    "BSV": bsv,
+    //"BTC": btc,
+	//"LTC": ltc,
 
-	"coins":["BTC", "LTC"]
+    "coins":["BSV",]
+	//"coins":["BSV","BTC", "LTC"]
 };
